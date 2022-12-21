12d5eaca825e01bb0e840dae7c4dbb2110dfde44
IngridAkeida <ingridsanchesp@gmail.com>
2022-12-21 18:24:02 +0100

    update commit

--- src/components/Header/styled_compenents.js
+++ src/components/Header/styled_compenents.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const StyledHeader = styled.div`
   background-color: ${({ theme }) => theme.backgroundLevel1};
+
   .user-banner{
     position: absolute;
     width: 100%;
@@ -9,13 +10,17 @@ const StyledHeader = styled.div`
     left: 0px;
     top: 56px;
     background-color: #ccc;
-    background-image: url(https://marketplace.canva.com/EAENvjvVCss/1/0/1600w/canva-corporate-professional-linkedin-banner-b_h10Jq8DkA.jpg);
+    background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZBw_VAe_gm6iUTDRXol00oBqdII2Yzd_zlA&usqp=CAU);
+    background-repeat: no-repeat;
+    background-size: 100%;
+    filter: blur(5px);
   }
   
   .user-perfil{
     width: 80px;
     height: 80px;
     border-radius: 50%;
+    border: solid red 1px;
   }
   .user-info{
     margin-top: 280px;
